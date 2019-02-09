4f634a8f95ef9fcf67398244afa757765b0fb701
Igor Knyazev <igor.s.knyazev@gmail.com>
2019-02-09 12:28:57 +0700

    Database model for storing words were added

---
+++ lib/make_word_bot/word.ex
@@ -0,0 +1,16 @@
+defmodule MakeWordBot.Word do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "words" do
+    field :word, :string
+  end
+
+  @doc false
+  def changeset(word, attrs) do
+    word
+    |> cast(attrs, [:word])
+    |> validate_required([:word])
+  end
+end
---
+++ priv/repo/migrations/20190209052533_create_words.exs
@@ -0,0 +1,10 @@
+defmodule MakeWordBot.Repo.Migrations.CreateWords do
+  use Ecto.Migration
+
+  def change do
+    create table(:words) do
+      add :word, :string
+    end
+
+  end
+end
