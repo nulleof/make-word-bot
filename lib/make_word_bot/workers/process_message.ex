77704a40139d872167ee9ba1e39a31b8713928e0
Igor Knyazev <igor.s.knyazev@gmail.com>
2019-02-16 19:51:02 +0700

    Working help was added

--- lib/make_word_bot/workers/process_message.ex
+++ lib/make_word_bot/workers/process_message.ex
@@ -46,6 +46,39 @@ defmodule MakeWordBot.ProcessMessage do
     end
   end
   
+  def process_help_message(chat_id) do
+    message = """
+    Это бот для игры в "Словослова"
+    
+    В этой игре тебе нужно составить из главного слова как можно больше слов поменьше, чтобы товарищи кусали локти от зависти, какой ты эрудированный!
+    
+    /start - чтобы начать игру в вашем чате. Игра длится 2 минуты
+    /score - чтобы показать счет в текущей игре
+    /word - чтобы показать главное слово в текущей игре
+    /help - чтобы показать эту подсказку еще раз
+    
+    Во время игры предлагайте варианты, просто отправляя их в чат. Чем длиннее слово, тем больше очков ты за него получишь! А, может, ты предпочитаешь зерг раш трехбуквенными словами? Твое дело! Побеждай и доминируй!
+    """
+    
+    send_message(chat_id, message)
+  end
+  
+  def process_start_message(chat_id) do
+  
+  end
+  
+  def process_score_message(chat_id) do
+
+  end
+
+  def process_word_message(chat_id) do
+
+  end
+  
+  def process_simple_message(chat_id, message_id, text, from) do
+  
+  end
+  
   def process_message(message) do
      message_id = message["message_id"]
      text = message["text"]
@@ -55,19 +88,27 @@ defmodule MakeWordBot.ProcessMessage do
      # detect where is this message from
      # I really need only ID, no matter is it type "private" or "group"
      chat_id = chat["id"]
+     chat_type = chat["type"]
+     
+     cond do
+       Regex.match?(~r[^/help@make_a_word_bot], text)
+       || (chat_type == "private" && Regex.match?(~r[^/help], text)) ->
+         process_help_message(chat_id)
+       
+       Regex.match?(~r[^/start@make_a_word_bot], text)
+       || (chat_type == "private" && Regex.match?(~r[^/start], text)) ->
+         process_start_message(chat_id)
   
-     case text do
-       # TODO: remove text constants from here (I'm about name)
-       n when n in ["/help", "/help@make_a_word_bot"] ->
-         send_message(chat_id, "Dis is a help, bro")
-    
-       n when n in ["/start", "/start@make_a_word_bot"] ->
-         send_message(chat_id, "Start it, bro")
-    
-       n when n in ["/score", "/score@make_a_word_bot"] ->
-         send_message(chat_id, "Yeah, dis is game score")
-    
-       word -> generate_and_send_message(chat_id, word, message_id, from)
+       Regex.match?(~r[^/score@make_a_word_bot], text)
+       || (chat_type == "private" && Regex.match?(~r[^/score], text)) ->
+         process_start_message(chat_id)
+  
+       Regex.match?(~r[^/word@make_a_word_bot], text)
+       || (chat_type == "private" && Regex.match?(~r[^/word], text)) ->
+         process_word_message(chat_id)
+         
+       true ->
+         process_simple_message(chat_id, message_id, text, from)
      end
   end
   
